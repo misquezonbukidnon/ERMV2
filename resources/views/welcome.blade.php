c1d67451280331b60fc6e3f21f435f15447d155d
MrRegei <cabugosregei@gmail.com>
2021-08-19 10:17:34 +0800

    081927
    ERM Corrections

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,6 +87,53 @@
     <script src="./assets/js/hs.chartjs.js"></script>
     <script src="./assets/js/hs.dropzone.js"></script>
 
+    <script src="./assets/vendor/datatables.net.extensions/select/select.min.js"></script>
+    <script src="./assets/js/hs.datatables.js"></script>
+
+
+
+    <script>
+      $(document).on('ready', function() {
+        // INITIALIZATION OF DATATABLES
+        // =======================================================
+        var datatable = $.HSCore.components.HSDatatables.init($('#columnSearchDatatable'));
+
+        $('#column1_search').on('keyup', function() {
+          datatable
+            .columns(0)
+            .search(this.value)
+            .draw();
+        });
+
+        $('#column2_search').on('change', function() {
+          datatable
+            .columns(1)
+            .search(this.value)
+            .draw();
+        });
+
+        $('#column3_search').on('change', function() {
+          datatable
+            .columns(2)
+            .search(this.value)
+            .draw();
+        });
+
+        $('#column4_search').on('change', function() {
+          datatable
+            .columns(3)
+            .search(this.value)
+            .draw();
+        });
+
+
+        // INITIALIZATION OF SELECT2
+        // =======================================================
+        $('.js-select2-custom').each(function() {
+          var select2 = $.HSCore.components.HSSelect2.init($(this));
+        });
+      });
+    </script>
 
 
     <script>
