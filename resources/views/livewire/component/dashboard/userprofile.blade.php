d651b4906fa4133498279b3c5cf94d6c026f4fc6
MrRegei <cabugosregei@gmail.com>
2021-08-24 15:09:50 +0800

    82421
    dashboard major changes

--- resources/views/livewire/component/dashboard/userprofile.blade.php
+++ resources/views/livewire/component/dashboard/userprofile.blade.php
@@ -1,51 +1,37 @@
- <div>
- <!-- Card -->
- <div class="js-sticky-block card mb-3 mb-lg-5" data-hs-sticky-block-options='{
-                        "parentSelector": "#accountSidebarNav",
-                        "breakpoint": "lg",
-                        "startPoint": "#accountSidebarNav",
-                        "endPoint": "#stickyBlockEndPoint",
-                        "stickyOffsetTop": 20
-                        }'>
-     <!-- ********************************************************************User Profile Overview (Can be converted to Component)************************************************* -->
-     <!-- Header -->
-     <div class="card-header">
-         <h5 class="card-header-title">Profile</h5>
-     </div>
-     <!-- End Header -->
+<div class="card">
+    <div class="card-header">
+        <h5 class="card-header-title">Profile</h5>
+    </div>
+    <div class="card-body">
+        <ul class="list-unstyled list-unstyled-py-3 text-dark mb-3">
+            <li class="py-0">
+                <small class="card-subtitle">About</small>
+            </li>
 
-     <!-- Body -->
-     <div class="card-body">
-         <ul class="list-unstyled list-unstyled-py-3 text-dark mb-3">
-             <li class="py-0">
-                 <small class="card-subtitle">About</small>
-             </li>
-
-             <li>
-                 <i class="tio-user-outlined nav-icon"></i>
-                 {{ $users->users->first }} {{ $users->users->last }}
-             </li>
+            <li>
+                <i class="tio-user-outlined nav-icon"></i>
+                {{ $users->users->first }} {{ $users->users->last }}
+            </li>
 
 
-             <li>
-                 <i class="tio-briefcase-outlined nav-icon"></i>
+            <li>
+                <i class="tio-briefcase-outlined nav-icon"></i>
                 {{ $users->offices->name }}
-             </li>
+            </li>
 
-             <li>
-                 <i class="tio-poi-user nav-icon"></i>
+            <li>
+                <i class="tio-poi-user nav-icon"></i>
                 {{ $users->positions->name }}
-             </li>
+            </li>
 
-             <li class="pt-2 pb-0">
-                 <small class="card-subtitle">Contacts</small>
-             </li>
+            <li class="pt-2 pb-0">
+                <small class="card-subtitle">Contacts</small>
+            </li>
 
-             <li>
-                 <i class="tio-online nav-icon"></i>
-                 {{ $users->users->email }}
-             </li>
-         </ul>
-     </div>
-     <!-- End Body -->
- </div>
+            <li>
+                <i class="tio-online nav-icon"></i>
+                {{ $users->users->email }}
+            </li>
+        </ul>
+    </div>
+</div>
