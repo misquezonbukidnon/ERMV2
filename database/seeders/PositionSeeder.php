5de4f8b657690bd23da0f9511f77f6fa35d73100
MrRegei <cabugosregei@gmail.com>
2021-08-25 11:36:07 +0800

    082521
    List of Positions

--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -15,12 +15,604 @@ class PositionSeeder extends Seeder
     public function run()
     {
         DB::table('positions')->insert([
-            [
-                'name' => 'Computer Programmer I',
-            ],
-            [
-                'name' => 'Admin Aide Clerk III',
-            ],
+        [
+            'name' => 'Administrative Aide I'
+        ],
+        [
+            'name' => 'Administrative Aide I (Crafts & Trade Helper)'
+        ],
+        [
+            'name' => 'Administrative Aide I (Laborer I)'
+        ],
+        [
+            'name' => 'Administrative Aide I (Utility Worker I)'
+        ],
+        [
+            'name' => 'Administrative Aide II (Bookbinder I)'
+        ],
+        [
+            'name' => 'Administrative Aide III'
+        ],
+        [
+            'name' => 'Administrative Aide III (Carpenter I)'
+        ],
+        [
+            'name' => 'Administrative Aide III (Clerk I)'
+        ],
+        [
+            'name' => 'Administrative Aide III (Clerk I)'
+        ],
+        [
+            'name' => 'Administrative Aide III (Driver I)'
+        ],
+        [
+            'name' => 'Administrative Aide III (Laborer II)'
+        ],
+        [
+            'name' => 'Administrative Aide III (Plumber I)'
+        ],
+        [
+            'name' => 'Administrative Aide III (Utility Worker II)'
+        ],
+        [
+            'name' => 'Administrative Aide IV'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Accounting Clerk I)'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Clerk II)'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Electrician I)'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Fiscal Clerk I)'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Human Resource Management Aide)'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Mechanic I)'
+        ],
+        [
+            'name' => 'Administrative Aide IV (Storekeeper I)'
+        ],
+        [
+            'name' => 'Administrative Aide VI'
+        ],
+        [
+            'name' => 'Administrative Aide VI (Accounting Clerk II)'
+        ],
+        [
+            'name' => 'Administrative Aide VI (Clerk III)'
+        ],
+        [
+            'name' => 'Administrative Aide VI (Communication Equipment Operator II)'
+        ],
+        [
+            'name' => 'Administrative Aide VI (Motorpool Dispatcher)'
+        ],
+        [
+            'name' => 'Administrative Assisitant III'
+        ],
+        [
+            'name' => 'Administrative Assistant II (Accounting Clerk III)'
+        ],
+        [
+            'name' => 'Administrative Assistant II (Budgeting Assistant)'
+        ],
+        [
+            'name' => 'Administrative Assistant III (Senior Bookkeeper)'
+        ],
+        [
+            'name' => 'Administrative Officer II (Accountant II)'
+        ],
+        [
+            'name' => 'Administrative Officer II (Budget Officer I)'
+        ],
+        [
+            'name' => 'Administrative Officer II (Information Officer I)'
+        ],
+        [
+            'name' => 'Administrative Officer II (Management and Audit Analyst I)'
+        ],
+        [
+            'name' => 'Administrative Officer III (Cashier II)'
+        ],
+        [
+            'name' => 'Administrative Officer IV'
+        ],
+        [
+            'name' => 'Agricultural Technologist'
+        ],
+        [
+            'name' => 'Agriculturist I'
+        ],
+        [
+            'name' => 'Assessment Clerk I'
+        ],
+        [
+            'name' => 'Assessment Clerk II'
+        ],
+        [
+            'name' => 'Assistant Registration Officer'
+        ],
+        [
+            'name' => 'Assistant Statistician I'
+        ],
+        [
+            'name' => 'Backhoe Operator'
+        ],
+        [
+            'name' => 'Barangay / Sports Coordination'
+        ],
+        [
+            'name' => 'Barangay Assistance Aide'
+        ],
+        [
+            'name' => 'Barangay Coordination'
+        ],
+        [
+            'name' => 'Barangay Health Aide'
+        ],
+        [
+            'name' => 'Bookkeeper I'
+        ],
+        [
+            'name' => 'Buyer II'
+        ],
+        [
+            'name' => 'Caretaking Service'
+        ],
+        [
+            'name' => 'Cemetery Caretaker'
+        ],
+        [
+            'name' => 'Clerical Service'
+        ],
+        [
+            'name' => 'Collection Service'
+        ],
+        [
+            'name' => 'Communication-In-Charge'
+        ],
+        [
+            'name' => 'Community Affairs Assistant I'
+        ],
+        [
+            'name' => 'Community Empowerment Facilitator'
+        ],
+        [
+            'name' => 'Computer Maintenance Technologist I'
+        ],
+        [
+            'name' => 'Computer Operation'
+        ],
+        [
+            'name' => 'Computer Operator / Emergency Response Service'
+        ],
+        [
+            'name' => 'Computer Operator I'
+        ],
+        [
+            'name' => 'Computer Programmer I'
+        ],
+        [
+            'name' => 'Construction Aide'
+        ],
+        [
+            'name' => 'Construction and Maintenance Foreman'
+        ],
+        [
+            'name' => 'Contact Tracing Aide'
+        ],
+        [
+            'name' => 'Contact Tracing Services'
+        ],
+        [
+            'name' => 'Courier Service'
+        ],
+        [
+            'name' => 'Database Manager'
+        ],
+        [
+            'name' => 'Day Care Worker I'
+        ],
+        [
+            'name' => 'Day Care Worker II'
+        ],
+        [
+            'name' => 'Dental Aide'
+        ],
+        [
+            'name' => 'Draftsman I'
+        ],
+        [
+            'name' => 'Draftsman II'
+        ],
+        [
+            'name' => 'Driving Services'
+        ],
+        [
+            'name' => 'Driving Services (Coaster)'
+        ],
+        [
+            'name' => 'Driving Services (Tractor)'
+        ],
+        [
+            'name' => 'Eco Aide'
+        ],
+        [
+            'name' => 'Electrical Service'
+        ],
+        [
+            'name' => 'Electrician I'
+        ],
+        [
+            'name' => 'Emergency Response Service'
+        ],
+        [
+            'name' => 'Emergency Response Supervision'
+        ],
+        [
+            'name' => 'Enforcement Assistance'
+        ],
+        [
+            'name' => 'Enforcement Service'
+        ],
+        [
+            'name' => 'Enforcer'
+        ],
+        [
+            'name' => 'Engineer I'
+        ],
+        [
+            'name' => 'Engineer II'
+        ],
+        [
+            'name' => 'Engineer III'
+        ],
+        [
+            'name' => 'Engineering Aide'
+        ],
+        [
+            'name' => 'Engineering Assistance'
+        ],
+        [
+            'name' => 'Engineering Service'
+        ],
+        [
+            'name' => 'Environmental Management Specialist II'
+        ],
+        [
+            'name' => 'Executive Assistant I'
+        ],
+        [
+            'name' => 'Executive Assistant II'
+        ],
+        [
+            'name' => 'Executive Assistant IV'
+        ],
+        [
+            'name' => 'Financial Analyst'
+        ],
+        [
+            'name' => 'Fire Aide'
+        ],
+        [
+            'name' => 'Garbage Collection'
+        ],
+        [
+            'name' => 'General Service Assistance'
+        ],
+        [
+            'name' => 'Groundskeeper'
+        ],
+        [
+            'name' => 'Health Aide Service'
+        ],
+        [
+            'name' => 'Heavy Equipment Helper'
+        ],
+        [
+            'name' => 'Heavy Equipment Operation'
+        ],
+        [
+            'name' => 'Heavy Equipment Operator I'
+        ],
+        [
+            'name' => 'Heavy Equipment Operator II'
+        ],
+        [
+            'name' => 'Helper'
+        ],
+        [
+            'name' => 'Housekeeping Service'
+        ],
+        [
+            'name' => 'Houseparent Service'
+        ],
+        [
+            'name' => 'Human Resource Management Assistant'
+        ],
+        [
+            'name' => 'Human Resource Management Officer II'
+        ],
+        [
+            'name' => 'Human Resource Management Officer III'
+        ],
+        [
+            'name' => 'Information Officer I'
+        ],
+        [
+            'name' => 'Internal Auditor II'
+        ],
+        [
+            'name' => 'IP Coordination'
+        ],
+        [
+            'name' => 'IT Assistance'
+        ],
+        [
+            'name' => 'Legislative Clerical Service'
+        ],
+        [
+            'name' => 'License Officer I'
+        ],
+        [
+            'name' => 'Lifesaving'
+        ],
+        [
+            'name' => 'Local Assessment Operation Officer I'
+        ],
+        [
+            'name' => 'Local Disaster Risk Reduction Management Assistant'
+        ],
+        [
+            'name' => 'Local Disaster Risk Reduction Management Officer II'
+        ],
+        [
+            'name' => 'Local Revenue Collection Officer II'
+        ],
+        [
+            'name' => 'Logistics-In-Charge'
+        ],
+        [
+            'name' => 'MAC Auxiliary'
+        ],
+        [
+            'name' => 'Maintenance'
+        ],
+        [
+            'name' => 'Maintenance Aide'
+        ],
+        [
+            'name' => 'Management & Audit Analyst II'
+        ],
+        [
+            'name' => 'Market Inspector I'
+        ],
+        [
+            'name' => 'Market Supervisor II'
+        ],
+        [
+            'name' => 'Mechanic II'
+        ],
+        [
+            'name' => 'Mechanic Service'
+        ],
+        [
+            'name' => 'Mechanic Service Aide'
+        ],
+        [
+            'name' => 'Medical Officer II'
+        ],
+        [
+            'name' => 'Medical Service'
+        ],
+        [
+            'name' => 'Medical Technologist II'
+        ],
+        [
+            'name' => 'Medtech Service'
+        ],
+        [
+            'name' => 'MGADH I (Assistant Municipal Engineer)'
+        ],
+        [
+            'name' => 'MGAHD I (Assistant Municipal Health Officer)'
+        ],
+        [
+            'name' => 'MGDH (Municipal Planning And Development Coordinator)'
+        ],
+        [
+            'name' => 'MGDH Municipal Engineer I'
+        ],
+        [
+            'name' => 'Midwife Assistance'
+        ],
+        [
+            'name' => 'Midwife I'
+        ],
+        [
+            'name' => 'Midwife II'
+        ],
+        [
+            'name' => 'Midwife III'
+        ],
+        [
+            'name' => 'MIdwifery Services'
+        ],
+        [
+            'name' => 'Municipal Accountant'
+        ],
+        [
+            'name' => 'Municipal Agriculturist'
+        ],
+        [
+            'name' => 'Municipal Budget Officer'
+        ],
+        [
+            'name' => 'Municipal Civil Registrar'
+        ],
+        [
+            'name' => 'Municipal Councilor'
+        ],
+        [
+            'name' => 'Municipal Government Assistant Department Head I'
+        ],
+        [
+            'name' => 'Municipal Health Officer'
+        ],
+        [
+            'name' => 'Municipal Mayor'
+        ],
+        [
+            'name' => 'Municipal Vice-Mayor'
+        ],
+        [
+            'name' => 'Nozzle Operation'
+        ],
+        [
+            'name' => 'Nurse II'
+        ],
+        [
+            'name' => 'Nursing Aide'
+        ],
+        [
+            'name' => 'Nursing Service'
+        ],
+        [
+            'name' => 'Nutrition Officer I'
+        ],
+        [
+            'name' => 'Nutrition Program Aide'
+        ],
+        [
+            'name' => 'Pharmacist II'
+        ],
+        [
+            'name' => 'Pharmacy Assistance'
+        ],
+        [
+            'name' => 'Pharmacy Services'
+        ],
+        [
+            'name' => 'Population Program Officer I'
+        ],
+        [
+            'name' => 'Population Program Worker II'
+        ],
+        [
+            'name' => 'Project Development Assistant I'
+        ],
+        [
+            'name' => 'Project Development Officer I'
+        ],
+        [
+            'name' => 'Project Engineering Services'
+        ],
+        [
+            'name' => 'Project Monitoring - Part Time'
+        ],
+        [
+            'name' => 'Purchasing Assistance'
+        ],
+        [
+            'name' => 'Record Officer I'
+        ],
+        [
+            'name' => 'Recorder'
+        ],
+        [
+            'name' => 'Registration Officer II'
+        ],
+        [
+            'name' => 'Revenue Collection Clerk II'
+        ],
+        [
+            'name' => 'Road Roller Operation'
+        ],
+        [
+            'name' => 'Sanitation Inspector III'
+        ],
+        [
+            'name' => 'Secretary to the Sanggunian'
+        ],
+        [
+            'name' => 'Security Agent I'
+        ],
+        [
+            'name' => 'Security Guard I'
+        ],
+        [
+            'name' => 'Security Service'
+        ],
+        [
+            'name' => 'Security Service'
+        ],
+        [
+            'name' => 'Sewing Assistance'
+        ],
+        [
+            'name' => 'Slaughtering Assistant'
+        ],
+        [
+            'name' => 'Social Welfare Aide'
+        ],
+        [
+            'name' => 'Social Welfare Assistant'
+        ],
+        [
+            'name' => 'Statistician Aide'
+        ],
+        [
+            'name' => 'Statistician I'
+        ],
+        [
+            'name' => 'Storekeeping'
+        ],
+        [
+            'name' => 'Street Sweeping'
+        ],
+        [
+            'name' => 'Surveying Service'
+        ],
+        [
+            'name' => 'Tailoring Instruction'
+        ],
+        [
+            'name' => 'Tire Repair Service'
+        ],
+        [
+            'name' => 'Tour Guide Service'
+        ],
+        [
+            'name' => 'Tourism Operations Officer I'
+        ],
+        [
+            'name' => 'Traffic Aide Service'
+        ],
+        [
+            'name' => 'Traffic Enforcement Service'
+        ],
+        [
+            'name' => 'Utility'
+        ],
+        [
+            'name' => 'Utility/Caretaking Service'
+        ],
+        [
+            'name' => 'Watchman Service'
+        ],
+        [
+            'name' => 'Welder I'
+        ],
+        [
+            'name' => 'Welding Service'
+        ],
+
         ]);
     }
 }
