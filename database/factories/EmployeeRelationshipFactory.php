4640be5e18b35a54c51e52cbe7b5779942708c8f
Management Information System Division Office Quezon Bukidnon <55367556+misquezonbukidnon@users.noreply.github.com>
2021-08-27 14:46:12 +0800

    082721
    Init for new models

---
+++ database/factories/EmployeeRelationshipFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\EmployeeRelationship;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmployeeRelationshipFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = EmployeeRelationship::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
