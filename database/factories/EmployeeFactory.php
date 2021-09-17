51d2fc91a71a8d03912602689b8a6c23896958c0
Jayson R. Alanano <technical.misquezonbukidnon@gmail.com>
2021-09-17 16:07:42 +0800

    BE 091721
    Employee Factory

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Employee;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class EmployeeFactory extends Factory
 {
@@ -22,7 +23,18 @@ class EmployeeFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'employment_date' => $this->faker->date('Y_m_d'),
+            'employee_number' => $this->faker->numerify('id-####'),
+            'lastname' => $this->faker->lastName(),
+            'firstname' => $this->faker->firstName(),
+            'middlename' => $this->faker->lastname(),
+            'suffix' => $this->faker->suffix(),
+            'address' => $this->faker->address(),
+            'contact_number' => $this->faker->phoneNumber(),
+            'email' => $this->faker->email(),
+            'emergency_contact_person' => $this->faker->name(),
+            'ecp_contact_number' => $this->faker->phoneNumber(),
+            'ecp_email' => $this->faker->email(),
         ];
     }
 }
--- database/factories/EmployeeRelationshipFactory.php
+++ database/factories/EmployeeRelationshipFactory.php
@@ -2,7 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\Classification;
+use App\Models\Employee;
 use App\Models\EmployeeRelationship;
+use App\Models\EmploymentStatus;
+use App\Models\Office;
+use App\Models\Position;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class EmployeeRelationshipFactory extends Factory
@@ -22,7 +27,11 @@ class EmployeeRelationshipFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'employees_id' => Employee::select('id')->orderByRaw("RAND()")->first()->id,
+            'offices_id' => Office::select('id')->orderByRaw("RAND()")->first()->id,
+            'positions_id' => Position::select('id')->orderByRaw("RAND()")->first()->id,
+            'classifications_id' => Classification::select('id')->orderByRaw("RAND()")->first()->id,
+            'employment_statuses_id' => EmploymentStatus::select('id')->orderByRaw("RAND()")->first()->id,
         ];
     }
 }
